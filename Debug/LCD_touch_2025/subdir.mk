a2e4b9f226e9cf330ea2f3d7488ff5ccffae754f
Jaime Fano Alijarde <875192@unizar.es>
2025-12-09 14:30:32 +0100

    Correcion Codex 1

--- Debug/LCD_touch_2025/subdir.mk
+++ Debug/LCD_touch_2025/subdir.mk
@@ -7,18 +7,21 @@ C_SRCS += \
 ../LCD_touch_2025/Bmp.c \
 ../LCD_touch_2025/lcd.c \
 ../LCD_touch_2025/main.c \
+../LCD_touch_2025/start_screen.c \
 ../LCD_touch_2025/tp.c 
 
 OBJS += \
 ./LCD_touch_2025/Bmp.o \
 ./LCD_touch_2025/lcd.o \
 ./LCD_touch_2025/main.o \
+./LCD_touch_2025/start_screen.o \
 ./LCD_touch_2025/tp.o 
 
 C_DEPS += \
 ./LCD_touch_2025/Bmp.d \
 ./LCD_touch_2025/lcd.d \
 ./LCD_touch_2025/main.d \
+./LCD_touch_2025/start_screen.d \
 ./LCD_touch_2025/tp.d 
 
 
